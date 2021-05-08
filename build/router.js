3c99a3018428a2d86c491cff43e4fc76272cf237
Udbhav Sahai <usahai@users.noreply.github.com>
2021-05-08 17:36:31 +0800

    updates to build

---
+++ build/router.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _HealthcheckController = _interopRequireDefault(require("./controllers/HealthcheckController"));
+
+var router = _express["default"].Router();
+
+router.use('/', _HealthcheckController["default"]);
+var _default = router;
+exports["default"] = _default;
